ec9fa84c0cfd8405cbc0a929f4e9962e040431bc
Satish-Dabhi <satidabhi555@gmail.com>
2025-06-18 18:46:02 +0530

    Updated the browse-route to resolve the route related issue in gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/satish-profile">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/projects" element={<Projects />} />
