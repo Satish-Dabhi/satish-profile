6feabc4586fd27d91b8190f3beb7ba8c7a4e9b96
Satish-Dabhi <satidabhi555@gmail.com>
2025-06-18 18:24:31 +0530

    Updated the vite cofig for resolve gh pages 404 error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { componentTagger } from "lovable-tagger";
-import { defineConfig } from "vite";
-import path from "path";
-import react from "@vitejs/plugin-react-swc";
+import { componentTagger } from 'lovable-tagger';
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react-swc';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig(({ mode }) => ({
   base: '/satish-profile/',
@@ -12,6 +13,18 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     mode === 'development' && componentTagger(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.html',
+          dest: '.', // this copies index.html → dist/404.html
+          rename: '404.html',
+        },
+      ],
+      watch: {
+        reloadPageOnChange: true, // ✅ fixed typo here
+      },
+    }),
   ].filter(Boolean),
   resolve: {
     alias: {
